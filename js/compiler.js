44f4a52d67501d66b9c245653328c596185f8f63
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2019-09-16 17:21:54 -0400

    Update compiler.js

--- js/compiler.js
+++ js/compiler.js
@@ -1,8 +1,10 @@
 var IS_INPUT_LINE = /\s*].+\s*/g;
 var MATCH_VARIABLES = /{[^{}]+}/g;
-var INPUT_FUNCTION_NAME = "___";
 
-function convertLine(sourceCodeArray, index) {
+var INPUT_FUNCTION_NAME = "___";
+var IMPORT_FUNCTION_NAME = "IMPORT(";
+
+function convertInputLines(sourceCodeArray, index) {
   var inputText = sourceCodeArray[index].match(IS_INPUT_LINE);
   if (inputText) {
     // There are inputs in this line that need to be replaced
@@ -17,3 +19,15 @@ function convertLine(sourceCodeArray, index) {
     sourceCodeArray[index] = parsedText;
   }
 }
+
+function convertImportLines(sourceCodeArray, index) {
+  var importStatement = sourceCodeArray[index].match(IMPORT_FUNCTION_NAME);
+  if (importStatement) {
+    var newText = sourceCodeArray[index].replace("IMPORT", "self.importScripts");
+    // Extensions arent required, so add them in here
+    newText = newText.replace(")", "+'.js')");
+    sourceCodeArray[index] = newText;
+  }
+}
+
+// The compiler function runs in a webworker
