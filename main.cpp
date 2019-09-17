184a6e067f28ced7f3238f2c593f2c62252190f8
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2019-09-17 09:37:43 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
-thing
+#include "third_party/CLI11.hpp"
+
+int main(int argc, char** argv) {
+    std::string description = "Zippy, a precompiling language to aid in the creation of TAS scripts";
+    std::string fileExtension = "zpy";
+      
+    CLI::App app{description};
+
+    std::string mainFileToParse = "main." + fileExtension;
+    app.add_option("-f,--file", mainFileToParse, "Main file of Zippy script");
+
+    CLI11_PARSE(app, argc, argv);
+  
+    return 0;
+}
