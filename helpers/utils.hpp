381f4aca75363a4b7e75cfd4e88ba5c8ed8b0f58
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2019-09-17 15:47:39 -0400

    Update utils.hpp

--- helpers/utils.hpp
+++ helpers/utils.hpp
@@ -29,4 +29,38 @@ bool replace(std::string& str, const std::string& from, const std::string& to) {
     str.replace(start_pos, from.length(), to);
     return true;
 }
+ 
+ /*! note: delimiter cannot contain NUL characters
+ */
+template <typename Range, typename Value = typename Range::value_type>
+std::string join(Range const& elements, const char *const delimiter) {
+    std::ostringstream os;
+    auto b = begin(elements), e = end(elements);
+
+    if (b != e) {
+        std::copy(b, prev(e), std::ostream_iterator<Value>(os, delimiter));
+        b = prev(e);
+    }
+    if (b != e) {
+        os << *b;
+    }
+
+    return os.str();
+}
+
+/*! note: imput is assumed to not contain NUL characters
+ */
+template <typename Input, typename Output, typename Value = typename Output::value_type>
+void split(char delimiter, Output &output, Input const& input) {
+    using namespace std;
+    for (auto cur = begin(input), beg = cur; ; ++cur) {
+        if (cur == end(input) || *cur == delimiter || !*cur) {
+            output.insert(output.end(), Value(beg, cur));
+            if (cur == end(input) || !*cur)
+                break;
+            else
+                beg = next(cur);
+        }
+    }
+}
 }
