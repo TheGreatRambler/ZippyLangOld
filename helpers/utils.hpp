9a984898dd4df3f5f1bc2e05236452150e4826bd
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2019-09-17 10:17:41 -0400

    Create utils.hpp

---
+++ helpers/utils.hpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+namespace UTILS {
+const std::string WHITESPACE = " \n\r\t\f\v";
+ 
+std::string ltrim(const std::string& s)
+{
+    size_t start = s.find_first_not_of(WHITESPACE);
+    return (start == std::string::npos) ? "" : s.substr(start);
+}
+ 
+std::string rtrim(const std::string& s)
+{
+    size_t end = s.find_last_not_of(WHITESPACE);
+    return (end == std::string::npos) ? "" : s.substr(0, end + 1);
+}
+ 
+std::string trim(const std::string& s)
+{
+    return UTILS::rtrim(UTILS::ltrim(s));
+}
+
+bool replace(std::string& str, const std::string& from, const std::string& to) {
+    size_t start_pos = str.find(from);
+    if(start_pos == std::string::npos)
+        return false;
+    str.replace(start_pos, from.length(), to);
+    return true;
+}
+}
