a652e1c555b8a3a9ed86527dbe9e638fd34ff25a
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2019-09-17 15:38:20 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 int main(int argc, char** argv) {
     std::string description = "Zippy, a precompiling language to aid in the creation of TAS scripts";
     std::string fileExtension = "zpy";
-      
+    
     CLI::App app{description};
 
     std::string mainFileToParse = "main." + fileExtension;
