19961119bd74034073c819058193059cdd59f7e5
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2019-09-17 10:26:49 -0400

    Update parser.hpp

--- parser.hpp
+++ parser.hpp
@@ -1,29 +1,52 @@
 #include <vector>
 #include <string>
 #include <regex>
+#include <fstream>
+
+#include "helpers/utils.hpp"
 
 std::regex IS_INPUT_LINE("\s*].+\s*");
 std::regex MATCH_VARIABLES("{[^{}]+}");
-/*
-function convertInputLines(sourceCodeArray, index) {
-  var inputText = sourceCodeArray[index].match(IS_INPUT_LINE);
-  if (inputText) {
-    // There are inputs in this line that need to be replaced
-    // Remove whitespace and ] and turn into function
-    var parsedText = INPUT_FUNCTION_NAME + "('" + inputText[0].trim().substr(1) + "');";
-    var variables = parsedText.match(MATCH_VARIABLES);
-    if (variables) {
-      variables.forEach(function(variable) {
-        parsedText = parsedText.replace(variable, "'+" + variable.slice(1,-1) + "+'");
-      });
+
+// This will become the inbuilt function to add an input
+std::string INPUT_FUNCTION_NAME = "ADD_INPUT";
+
+std::string ConvertInputLinesIfNeeded(std::string stringToParse) {
+  std::smatch matches;
+  if (std::regex_match(&stringToParse, matches, IS_INPUT_LINE) {
+    // There is a match
+    std::string match = matches[0].str();
+    //Build string
+    std::string parsedText = INPUT_FUNCTION_NAME + "('" + UTILS::trim(match).substr(1) + "');";
+    std::smatch variableMatches;
+    if (std::regex_match(&parsedText, variableMatches, MATCH_VARIABLES)) {
+      for (int i = 0; i < variableMatches.size(); i++) {
+        std::string thisMatch = variableMatches[i].str();
+        // Removes first and last char
+        UTILS::replace(parsedText, thisMatch, "'+" + thisMatch.substr(1, thisMatch.size() - 2) + "+'");
+      }
     }
-    sourceCodeArray[index] = parsedText;
+    return parsedText;
   }
+  // This is not an input line
+  returnstringToParse;
 }
-*/
 
-void ConvertInputLinesIfNeeded(std::vector<std::string>* 
-
+// Parse the lines to account for input lines
 std::vector<std::string> ParseScript(std::string filename) {
-
+  // The lines
+  std::vector<std::string> scriptLines;
+  std::ifstream fileStream(filename);
+  if (fileStream.is_open()) {
+    std::string line;
+    while (std::getline(fileStream, line)) {
+        // `line` is the current line
+        // Now it is converted
+        std::string parsedLine = ConvertInputLinesIfNeeded(line);
+        // Add to script lines
+        scriptLines.push_back(parsedLine);
+    }
+    file.close();
+  }
+  return scriptLines;
 }
