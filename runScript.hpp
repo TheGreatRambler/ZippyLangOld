46b6e20f368e40ce23288f03c17ef24edefd1969
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2019-09-17 16:09:38 -0400

    Update runScript.hpp

--- runScript.hpp
+++ runScript.hpp
@@ -1,10 +1,29 @@
 #include "QuickJS/quickjs.h"
 
+#include "helpers/utils.hpp"
+
 #include "parser.hpp"
 
-JSRuntime* currentJSRuntime = JS_NewRuntime();
-JSContext* currentJSContext = JS_NewContext(currentJSRuntime);
+JSRuntime* currentJSRuntime;
+JSContext* currentJSContext;
 
-void startRunning(std::string filename) {
-  std::vector<std::string> lines = 
+std::vector<std::string> generatedScript;
+
+JSValue AddLine(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv) 
+
+void RunScript(std::string filepath) {
+  // Will be called recursively if the script does so
+  std::vector<std::string> lines = ParseScript(filepath);
+  // Parsed lines will now be executed
+  // Join them together
+  std::string script = UTILS::joinVectorOfStrings(lines, "\n");
+  // Run the code in the context
+  JS_Eval(currentJSContext, script.c_str(), script.size(), filepath, JS_EVAL_TYPE_GLOBAL);
+}
+
+void StartEvaluating(std::string filepath) {
+  // Create runtime
+  currentJSRuntime = JS_NewRuntime();
+  // Create context
+  currentJSContext = JS_NewContext(currentJSRuntime);
 }
