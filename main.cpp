af8c30a3f250facf272b243189d09387f7eb3787
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2019-09-17 17:21:44 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <string>
 
+#include "helpers/utils.hpp"
+
 #include "third_party/CLI11.hpp"
 
-#include "parser.hpp"
+#include "runScript.hpp"
 
 int main(int argc, char** argv) {
     std::string description = "Zippy, a precompiling language to aid in the creation of TAS scripts";
@@ -12,8 +14,20 @@ int main(int argc, char** argv) {
 
     std::string mainFileToParse = "main." + fileExtension;
     app.add_option("-f,--file", mainFileToParse, "Main file of Zippy script");
+    
+    std::string nameOfGeneratedFile = "generated.txt";
+    app.add_option("-n,--name", nameOfGeneratedFile, "Name to give to generated script");
+    
+    std::string folder = UTILS::getcwd_string();
+    app.add_option("-d,--dir", folder, "Directory to run from and cwd otherwise");
 
     CLI11_PARSE(app, argc, argv);
-  
+    
+    // Start running the function
+    // Will block entire program while compiling
+    StartEvaluating(mainFileToParse, folder);
+    // Write generated file because parsing is done
+    WriteGeneratedFile(nameOfGeneratedFile);
+    
     return 0;
 }
